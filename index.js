8d9f81295b2dbe0784b3a00f9196ef1d14ca26b3
Abhishek kushwaha <96886598+AKBUA@users.noreply.github.com>
2024-03-14 12:23:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["https://kuvakchatclient.onrender.com/","http://localhost:3000/"],
+    origin: ["https://kuvakchatclient.onrender.com","http://localhost:3000"],
     methods: ["GET", "POST"]
   },
 });
